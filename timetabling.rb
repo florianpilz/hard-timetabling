d882878b6c385ac296e9f3665b637be66addebee
Florian Pilz <fpilz87@googlemail.com>
2011-01-24 15:49:35 +0100

    remove old code for binary mutation and population

--- timetabling.rb
+++ timetabling.rb
@@ -11,12 +11,6 @@ class Individual
     
     @periods[@rand_period_nr1].constraints[@rand_constraint_nr1], @colliding_periods[@rand_period_nr2].constraints[@rand_constraint_nr2] =
       @colliding_periods[@rand_period_nr2].constraints[@rand_constraint_nr2], @periods[@rand_period_nr1].constraints[@rand_constraint_nr1]
-
-    # constraint = @periods[@rand_period_nr1].constraints[@rand_constraint_nr1]
-    # constraint.klass = rand(4) + 1
-    # constraint.teacher = rand(4) + 1
-    # constraint.room = rand(4) + 1
-    
     self.update
   end
 end
@@ -51,65 +45,6 @@ def hillclimber(individual)
   iterations
 end
 
-def population_based_hillclimber(individual, size)
-  iterations = 0
-  population = []
-  size.times { population << individual.deep_clone }
-  population.each do |i|
-    i.periods = i.periods.sort_by{rand}
-  end
-  
-  best = select_best(population)
-  while best.collisions > 0
-    childs = []
-    population.each do |i|
-      child = i.deep_clone
-      child.mutate
-      childs << child
-    end
-    
-    selection_population = population + childs
-    selection_population = selection_population.sort_by{|i| i.collisions}.reverse
-    selection = []
-    population.length.times {|i| selection << selection_population.pop}
-    population = selection
-    best = select_best(population)
-    iterations += 1
-    puts "Iterations: #{iterations}, collisions of best individual: #{best.collisions}" if iterations % 1000 == 0
-  end
-  iterations
-end
-
-def select_best(population)
-  best = population.first
-  population.each do |individual|
-    if individual.collisions < best.collisions
-      best = individual
-    end
-  end
-  best
-end
-
-def exchange(p1, p2, pn1, cn1, pn2, cn2)
-  p1[pn1].constraints[cn1], p2[pn2].constraints[cn2] = p2[pn2].constraints[cn2], p1[pn1].constraints[cn1]
-end
-
-def problem_creator(depth)
-  all_constraints = []
-  periods = []
-  depth.times do |i|
-    i += 1
-    constraints = []
-    depth.times do
-      constraints << Constraint.new(:klass => i, :teacher => i, :room => i)
-    end
-    periods << Period.new(:constraints => constraints)
-    all_constraints << constraints
-  end
-  all_constraints = all_constraints.flatten
-  Individual.new(periods, all_constraints)
-end
-
 def parse_constraint(text_constraint)
   klass, teacher, room = text_constraint.scan(/C(\d).*S\d.*T(\d).*R(\d).*/).first.map!{ |number_as_string| number_as_string.to_i }
   Constraint.new(:klass => klass, :teacher => teacher, :room => room)
@@ -132,16 +67,13 @@ File.open("hard-timetabling-data/hdtt4list.txt", "r") do |file|
     period_constraints = []
     rooms.times do |j|
       period_constraints << constraints[i * rooms + j]
-      # period_constraints << Constraint.new(:klass => 1, :teacher => 1, :room => 1) # for binary-mutation approach
     end
     periods << Period.new(:constraints => period_constraints)
   end
   
   individual = Individual.new(periods, constraints)
-  # individual = problem_creator(5)
   
   iterations = hillclimber(individual)
-  # iterations = population_based_hillclimber(individual, 5)
   puts "Iterations for random solver: #{iterations}"
   puts "Runtime in seconds: #{Time.new - time}"
 end
