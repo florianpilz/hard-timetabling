98b51e11e75e2194de10f1d369029b4283c1f651
Florian Pilz <fpilz87@googlemail.com>
2011-01-31 14:57:02 +0100

    start refactoring
    - remove Period class
    - remove commented code
    - accept mutation and recombination as Procs wrapped in a class
    - enable definition of collision-granularity
    - enable debug mode to check for unfulfilled constraints
    - rewrite calculation of collisions
    - rewrite deep_clone method and rename it to copy

--- base.rb
+++ base.rb
@@ -1,3 +1,5 @@
+NUMBER_OF_SLOTS = 30 # same for all sample data
+
 class Array
   def sum
     inject( nil ) { |sum,x| sum ? sum+x : x }
@@ -17,64 +19,41 @@ class Constraint
     @room = values[:room]
   end
   
-  def deep_clone
-    self
-    # self.clone
-  end
-  
   def to_s
     "Klasse: #{@klass}, Lehrer: #{@teacher}, Raum: #{@room}"
   end
 end
 
-class Period
+class Individual
   attr_accessor :constraints
   
   def initialize(values = {})
-    @constraints = values[:constraints]
-  end
-
-  def collisions
-    collisions = 0
-    @constraints.each do |c1|
-      @constraints.each do |c2|
-        next if c1 == c2
-        # collisions += 1 if c1.klass == c2.klass or c1.teacher == c2.teacher or c1.room == c2.room
-        collisions += 1 if c1.klass == c2.klass
-        collisions += 1 if c1.teacher == c2.teacher
-        collisions += 1 if c1.room == c2.room
-      end
-    end
-    collisions
+    values = {:granularity => 0, :debug => false}.merge(values)
+    @constraints          = values[:current_constraints]
+    @expected_constraints = values[:expected_constraints]
+    @mutation             = values[:mutation]
+    @recombination        = values[:recombination]
+    @granularity          = values[:granularity]
+    @debug                = values[:debug]
+    @slot_size            = values[:slot_size]
   end
   
-  def deep_clone
-    clone = self.clone
-    clone.constraints = self.constraints.map{|c| c.deep_clone}
-    clone
-  end
-
   def to_s
-    i = 0
-    @constraints.inject("") do |output, c|
-      i += 1
-      output << "Constraint #{i}: Klasse = #{c.klass}, Lehrer = #{c.teacher}, Raum = #{c.room}\n"
-    end
+    "Individual with mutation #{@mutation.name} and recombination #{@recombination.name}"
   end
-end
-
-class Individual
-  attr_accessor :periods, :colliding_periods, :constraints
   
-  def initialize(periods, constraints)
-    @periods = periods.map{|p| p.deep_clone}
-    @constraints = constraints.map{|c| c.deep_clone}
-    @colliding_periods = @periods.select{|p| p.collisions > 0}
-    @old_colliding_periods = @colliding_periods
-    # @rand_period_nr1 = 0
-    # @rand_period_nr2 = 0
-    # @rand_constraint_nr1 = 0
-    # @rand_constraint_nr2 = 0
+  def copy
+    individual = self.clone
+    individual.constraints = @constraints.clone # only attribute that may change
+    individual
+  end
+  
+  def mutate
+    @mutation.call(self)
+  end
+  
+  def recombinate_with(individual)
+    @recombination.call(self, individual)
   end
   
   def fitness
@@ -82,47 +61,50 @@ class Individual
   end
   
   def collisions
-    @colliding_periods.inject(0){|sum, p| sum += p.collisions}
-    # @colliding_periods.length
-  end
-  
-  def unfulfilled_constraints # FIXME yields 0 if two identical constraints only occur once
-    temp_constraints = @constraints.map{|c| c.deep_clone}
-    delete_constraint = nil
-    @periods.each do |period|
-      period.constraints.each do |c1|
-        temp_constraints.each do |c2|
-          if c1.klass == c2.klass and c1.teacher == c2.teacher and c1.room == c2.room
-            delete_constraint = c2
-            break
+    collisions = 0
+    NUMBER_OF_SLOTS.times do |n|
+      old_collisions = collisions
+      
+      0.upto(@slot_size - 1) do |i|
+        c1 = @constraints[@slot_size * n + i]
+        
+        (i + 1).upto(@slot_size - 1) do |j|
+          c2 = @constraints[@slot_size * n + j]
+
+          if @granularity == 0
+            collisions += 1 if c1.klass == c2.klass
+            collisions += 1 if c1.teacher == c2.teacher
+            collisions += 1 if c1.room == c2.room
+          elsif @granularity == 1
+            collisions += 1 if c1.klass == c2.klass || c1.teacher == c2.teacher || c1.room == c2.room
+          else
+            if old_collisions == collisions # only increase collisions once per period
+              collisions += 1 if c1.klass == c2.klass || c1.teacher == c2.teacher || c1.room == c2.room
+            end
           end
         end
-        temp_constraints.delete(delete_constraint) if delete_constraint != nil
       end
     end
-    temp_constraints.length
+    collisions
   end
   
-  def deep_clone
-    clone = self.clone
-    # clone.periods = Marshal.load(Marshal.dump(@periods)) # short and safe, but expensive
-    clone.periods = self.periods.map{|p| p.deep_clone}
-    clone.update # FIXME should not update in clone method
-    clone
-  end
-  
-  def update
-    @old_colliding_periods = @colliding_periods
-    @colliding_periods = @periods.select{|p| p.collisions > 0}
-  end
+  def unfulfilled_constraints
+    return 0 unless debug # assume all constraints are fulfilled unless in debug mode
     
-  # def print_last_mutation # TODO not used anymore
-  #   unless @old_colliding_periods.include?(@periods[@rand_period_nr1]) # TODO why is this necessary?
-  #     puts "Colliding (#{@rand_constraint_nr2}):"
-  #     puts @old_colliding_periods[@rand_period_nr2]
-  #     puts ""
-  #     puts "Other (#{@rand_constraint_nr1}):"
-  #     puts @periods[@rand_period_nr1].to_s
-  #   end
-  # end
+    # FIXME yields 0 if two identical constraints only occur once => should be solved
+    expected_constraints = @expected_constraints.clone
+    delete_constraints = []
+    
+    expected_constraints.each do |c1|
+      constraints.each do |c2|
+        delete_constraints << c1 if c1 == c2
+      end
+    end
+    
+    delete_constraints.each do |c|
+      expected_constraints.delete(c)
+    end
+    
+    expected_constraints.length
+  end
 end
